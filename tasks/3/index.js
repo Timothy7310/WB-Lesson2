9e15b45486b39121085363529e850ac25736dc7d
Timothy7310 <penlist127@gmail.com>
2023-08-10 14:31:48 +0300

    feat: add 3 task

---
+++ tasks/3/index.js
@@ -0,0 +1,47 @@
+const mathX = () => {
+  const getFibonachiNumber = (number) => {
+    if (number <= 1) {
+      return number;
+    }
+    let numberArr = [...Array.from(Array(number), (_, index) => index), number];
+
+    for (let i = 2; i < numberArr.length; i += 1) {
+      numberArr[i] = numberArr[i - 1] + numberArr[i - 2];
+    }
+
+    return numberArr[number];
+  };
+
+  const getAllFibonachiNumbers = (number) => {
+    return Array.from(Array(number), (_, index) => index).map((number) =>
+      getFibonachiNumber(number)
+    );
+  };
+
+  const isPrime = (number) => {
+    for (let i = 2; i < number; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return number > 1;
+  };
+
+  const getAllPrimeNumbers = (number) => {
+    return Array.from(Array(number), (_, index) => index + 1).filter((number) =>
+      isPrime(number)
+    );
+  };
+
+  return {
+    isPrime,
+    getAllPrimeNumbers,
+    getFibonachiNumber,
+    getAllFibonachiNumbers,
+  };
+};
+
+const some = mathX();
+
+console.log(some.getFibonachiNumber(6));
+console.log(some.getAllFibonachiNumbers(10));

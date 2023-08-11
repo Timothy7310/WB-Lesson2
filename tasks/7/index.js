d7fa67ec3b45181c65a151e1ca0527502aec5641
Timothy7310 <penlist127@gmail.com>
2023-08-11 18:34:38 +0300

    feat: add 7 task

---
+++ tasks/7/index.js
@@ -0,0 +1,43 @@
+const testFn1 = () => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve("testFn1");
+    }, 2000)
+  );
+};
+const testFn2 = () => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve("testFn2");
+    }, 1000)
+  );
+};
+const testFn3 = () => {
+  return new Promise((resolve) => resolve("testFn3"));
+};
+const testFn4 = () => {
+  return "testFn4";
+};
+const fnArray = [testFn1, testFn2, testFn3, testFn4];
+
+// с циклом for
+async function runAllFunction(functions) {
+  let fnResult;
+
+  for (let i = 0; i < functions.length; i += 1) {
+    fnResult = await functions[i]();
+    console.log(fnResult, `index of function is ${i}`);
+  }
+}
+
+// с циклом for of
+async function runAllFunction2(functions) {
+  let fnResult;
+
+  for (const [index, fn] of functions.entries()) {
+    fnResult = await fn();
+    console.log(fnResult, `index of function is ${index}`);
+  }
+}
+
+runAllFunction(fnArray);

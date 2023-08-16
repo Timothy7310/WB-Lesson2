7972ecb909b3417645c6409a078c88aa04687b48
Timothy7310 <penlist127@gmail.com>
2023-08-16 10:58:13 +0300

    feat: add 4 task

--- tasks/4/index.js
+++ tasks/4/index.js
@@ -3,26 +3,106 @@ const pluralWord = (count, word) => {
   const form = pr.select(count);
 
   const suffixes1 = new Map([
+    ["zero", ""],
+    ["one", "а"],
+    ["few", "ы"],
+    ["many", ""],
+  ]);
+
+  const suffixes2 = new Map([
+    ["zero", "й"],
+    ["one", "я"],
+    ["few", "и"],
+    ["many", "й"],
+  ]);
+
+  const suffixes3 = new Map([
     ["zero", "й"],
     ["one", "е"],
     ["few", "я"],
     ["many", "й"],
   ]);
 
-  const suffixes2 = new Map([
+  const suffixes4 = new Map([
+    ["zero", ""],
+    ["one", "о"],
+    ["few", "а"],
+    ["many", ""],
+  ]);
+
+  const suffixes5 = new Map([
     ["zero", "ей"],
     ["one", "ь"],
     ["few", "я"],
     ["many", "ей"],
   ]);
 
-  if (word.at(-1) === "е") {
+  const suffixes6 = new Map([
+    ["zero", "гов"],
+    ["one", "г"],
+    ["few", "га"],
+    ["many", "гов"],
+  ]);
+
+  // мужского рода и женского рода с окончаниями -а
+  if (word.at(-1) === "а") {
     return `${count} ${word.slice(0, -1)}${suffixes1.get(form)}`;
   }
 
-  if (word.at(-1) === "ь") {
+  // мужского рода и женского рода с окончаниями -я
+  if (word.at(-1) === "я") {
     return `${count} ${word.slice(0, -1)}${suffixes2.get(form)}`;
   }
+
+  //  среднего рода с окончанием -e
+  if (word.at(-1) === "е") {
+    return `${count} ${word.slice(0, -1)}${suffixes3.get(form)}`;
+  }
+
+  //  среднего рода с окончанием -о
+  if (word.at(-1) === "о") {
+    return `${count} ${word.slice(0, -1)}${suffixes4.get(form)}`;
+  }
+
+  //  мужского рода с окончанием -ь
+  if (word.at(-1) === "ь") {
+    return `${count} ${word.slice(0, -1)}${suffixes5.get(form)}`;
+  }
+
+  // мужской род с нулевым окончанием долг
+  return `${count} ${word.slice(0, -1)}${suffixes6.get(form)}`;
 };
 
-console.log(pluralWord(121, "конь"));
+console.log(pluralWord(0, "страна"));
+console.log(pluralWord(0, "авария"));
+console.log(pluralWord(0, "сообщение"));
+console.log(pluralWord(0, "яблоко"));
+console.log(pluralWord(0, "пользователь"));
+console.log(pluralWord(0, "долг"));
+
+console.log("\n");
+
+console.log(pluralWord(1, "страна"));
+console.log(pluralWord(1, "авария"));
+console.log(pluralWord(1, "сообщение"));
+console.log(pluralWord(1, "яблоко"));
+console.log(pluralWord(1, "пользователь"));
+console.log(pluralWord(1, "долг"));
+
+console.log("\n");
+
+console.log(pluralWord(2, "страна"));
+console.log(pluralWord(2, "авария"));
+console.log(pluralWord(2, "сообщение"));
+console.log(pluralWord(2, "яблоко"));
+console.log(pluralWord(2, "пользователь"));
+console.log(pluralWord(2, "долг"));
+
+console.log("\n");
+
+console.log(pluralWord(5, "страна"));
+console.log(pluralWord(5, "авария"));
+console.log(pluralWord(5, "сообщение"));
+console.log(pluralWord(5, "яблоко"));
+console.log(pluralWord(5, "пользователь"));
+console.log(pluralWord(5, "долг"));

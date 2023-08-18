fb5b4f03b37de3f40fff765d6bb209d7047d5834
Timothy7310 <penlist127@gmail.com>
2023-08-18 17:57:02 +0300

    feat: add 23 task

---
+++ tasks/23/index.js
@@ -0,0 +1,10 @@
+const generateWrite = () => {
+  document.write(document.querySelector("#test").innerHTML);
+};
+
+const showCount = () => {
+  const count = document.querySelectorAll(".test").length;
+  const root = document.querySelector("#count-num");
+
+  root.textContent = count;
+};
